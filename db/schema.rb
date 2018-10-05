89698795b6b5e2c6b5f091ba2e963d5d93062784
d_riko1130 <free.and.freedom1130@gmail.com>
2018-10-05 17:10:17 +0900

    join_signin-out

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20181005055113) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "user",                                null: false
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
