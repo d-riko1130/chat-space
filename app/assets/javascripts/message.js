cf4188376dc61abb79723fafb081c91e1582ee63
d_riko1130 <free.and.freedom1130@gmail.com>
2018-10-11 12:18:37 +0900

    Action message#create as son

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,52 @@
-$(function(){
+$(function() {
+  // function buildHTML(message) {
+  //   var html = `
+
+  //   <div class="message">
+  //   <div class="upper-message">
+  //   <div class="upper-message__user-name">
+  //   <%= message.user.name %>
+  //   </div>
+  //   <div class="upper-message__date">
+  //   <%= message.created_at.strftime('%Y/%m/%d %H:%M') %>
+  //   </div>
+  //   </div>
+  //   <div class="lower-message">
+  //   <% if message.content.present? %>
+  //   <p class="lower-message__content">
+  //   <%= message.content %>
+  //   </p>
+  //   <%= image_tag message.image.url, class: 'lower-message__image' if message.image.present? %>
+  //   </div>
+  //   </div>
+
+  //   `
+  //   return html;
+  // }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
+    var url = window.location.href
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    // .done(function(data) {
+    //   console.log('おっけー')
+    //   var html = buildHTML(data);
+    //   $('.message').append(html);
+    //   $('.form__message').val('');
+    //   $('.form__mask_image').val('');
+    //   // $('.form__submit').prop('disabled', false);
+    // })
+    // .fail(function(message){
+    //   console.log('えらー');
+    // });
   })
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,13 +17,24 @@ class MessagesController < ApplicationController
     end
   end
 
-  private
+#   respond_to do |format|
+#     if @message.save
+#       format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+#       format.json
+#     else
+#       @messages = @group.messages.includes(:user)
+#       flash.now[:alert] = 'メッセージを入力してください。'
+#       render :index
+#     end
+#   end
+# end
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-  end
+private
+def message_params
+  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+end
 
-  def set_group
-    @group = Group.find(params[:group_id])
-  end
+def set_group
+  @group = Group.find(params[:group_id])
+end
 end
