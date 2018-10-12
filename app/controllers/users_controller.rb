824bddd21dbb7926e1d808a1b0f6fcb234f8a0df
d_riko1130 <free.and.freedom1130@gmail.com>
2018-10-12 20:38:34 +0900

    Set API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all(user_params)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
@@ -14,7 +19,6 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email)
   end
