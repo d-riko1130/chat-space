bf46068ffeada8e065af8a663231aaa7ef091018
d_riko1130 <free.and.freedom1130@gmail.com>
2018-10-14 23:08:54 +0900

    join_search-user

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,59 +1,51 @@
 $(function() {
-  var search_user = $(".chat-group-user");
-  var preWord;
+  var search_user = $("#user-search-result");
 
+  // 検索結果が一致したときのHTML
   function appendUser(user) {
-    var html = `
-    <div class="chat-group-user clearfix">
+    var html =
+    `<div class="chat-group-user clearfix">
     <p class="chat-group-user__name">${ user.name }</p>
     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
-    </div>
-    `
+    </div>`
     search_user.append(html);
   }
 
+  // 検索結果が一致しないときのHTML
   function appendNoUser(user) {
     var html = `
     <div class="chat-group-user clearfix">
-    <p class="chat-group-user__name">一致するユーザはいません</p>
-    </div>
-    `
+    <p class="chat-group-user__name">${ user }</p>
+    </div>`
     search_user.append(html);
   }
 
-  function editElement(element) {
-    var result = "^" + element;
-    return result;
-  }
-
+  // ajax通信
   $('#user-search-field').on('keyup', function() {
     var input = $('#user-search-field').val();
-    var inputs = input.split(" ");
-    var newInputs = inputs.map(editElement);
-    var word = newInputs.join("|");
-    var reg = RegExp(word);
 
     $.ajax({
       type: 'GET',
       url: '/users',
-      data: { keyword: input },
+      data: { name: input },
       dataType: 'json'
     })
 
-    .done(function(user_name){
-      $("#user-search-field").empty();
-      if ( user_name.length !== 0 ) {
-        user_name.forEach(function(user_name) {
-          appendUser(user_name);
+    // ajax通信が成功
+    .done(function(users){
+      $("#user-search-result").empty();
+      if ( users.length !== 0 ) {
+        users.forEach(function(user) {
+          appendUser(user);
         });
       } else {
         appendNoUser("一致するユーザはいません");
       }
     })
 
+    // ajax通信が失敗
     .fail(function() {
       alert('ユーザの検索に失敗しました')
-      console.log('シッパイシチャッタ')
     });
   });
 });
