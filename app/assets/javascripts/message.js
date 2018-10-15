627953f3d2a489bc659cb14d2c02fbb906819cee
d_riko1130 <free.and.freedom1130@gmail.com>
2018-10-15 14:21:52 +0900

    join_incremental-search
    # what
    インクリメンタルサーチによるユーザ検索およびユーザ追加・削除機能を実装
    
    # why
    グループ作成時のメンバー選択を簡易にするため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
 $(function() {
+  // 投稿するメッセージのHTML
   function buildHTML(message) {
     var content = message.content ? `<p class="lower-message__content">${ message.content }</p>` : ''
     var imageurl = message.image.url ? `<img src="${message.image.url}", class="lower-message__image">` : ''
@@ -18,6 +19,7 @@ $(function() {
                 </div>`
     return html
   };
+  // ajax通信でmessage#createを動かす
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -30,15 +32,19 @@ $(function() {
       processData: false,
       contentType: false
     })
+
+    // ajax通信が成功
     .done(function(data) {
       var html = buildHTML(data);
       $('.messages').append(html);
       $('#new_message')[0].reset();
       $('.messages').animate( {'scrollTop': $('.messages')[0].scrollHeight}, 1000 );
     })
+    // ajax通信が失敗
     .fail(function(message) {
       alert('メッセージの送信に失敗しました');
     })
+    // ajax通信の成功・失敗にかかわらず作動
     .always(function() {
       $('.form__submit').prop('disabled', false);
     });
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -13,17 +13,27 @@ $(function() {
 
   // 検索結果が一致しないときのHTML
   function appendNoUser(user) {
-    var html = `
-    <div class="chat-group-user clearfix">
+    var html =
+    `<div class="chat-group-user clearfix">
     <p class="chat-group-user__name">${ user }</p>
     </div>`
     search_user.append(html);
   }
 
-  // ajax通信
+  // ユーザ追加するときのHTML
+  function appendMember(id, name) {
+    var html =
+    `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ id }'>
+    <input name='group[user_ids][]' type='hidden' value='${ id }'>
+    <p class='chat-group-user__name'>${ name }</p>
+    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id='${ id }'>削除</a>
+    </div>`
+    return html
+  }
+
+  // ajax通信でusers#indexを動かす
   $('#user-search-field').on('keyup', function() {
     var input = $('#user-search-field').val();
-
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -42,10 +52,24 @@ $(function() {
         appendNoUser("一致するユーザはいません");
       }
     })
-
     // ajax通信が失敗
     .fail(function() {
       alert('ユーザの検索に失敗しました')
     });
+
+    // メンバーを追加する
+    $('#user-search-result').on('click', '.user-search-add', function() {
+      var id = $(this).data('userId')
+      var name = $(this).data('userName');
+      var insertMember = appendMember(id, name);
+      $('#chat-group-users').append(insertMember);
+      $(this).parent('.chat-group-user').remove();
+    });
+
+    // メンバーを削除する
+    $('#chat-group-users').on('click', '.user-search-remove', function() {
+      var id = $(this).data('userId');
+      $(`#chat-group-user-${id}`).remove();
+    })
   });
 });
