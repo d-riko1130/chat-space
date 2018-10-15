5d7e35e50bb893af63c6ff27b86b900b78822486
d_riko1130 <free.and.freedom1130@gmail.com>
2018-10-15 14:40:34 +0900

    Fix comment out

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,22 @@
 $(function() {
-  // 投稿するメッセージのHTML
   function buildHTML(message) {
     var content = message.content ? `<p class="lower-message__content">${ message.content }</p>` : ''
     var imageurl = message.image.url ? `<img src="${message.image.url}", class="lower-message__image">` : ''
-    var html = `<div class="message">
-                  <div class="upper-message">
-                    <div class="upper-message__user-name">
-                      ${ message.name }
-                    </div>
-                    <div class="upper-message__date">
-                      ${ message.date }
-                    </div>
-                  </div>
-                  <div class="lower-message">
-                    ${ content }
-                    ${ imageurl }
-                  </div>
-                </div>`
+    var html = `
+    <div class="message">
+      <div class="upper-message">
+        <div class="upper-message__user-name">${ message.name }</div>
+        <div class="upper-message__date">${ message.date }</div>
+      </div>
+      <div class="lower-message">
+        ${ content }
+        ${ imageurl }
+      </div>
+    </div>
+    `
     return html
   };
-  // ajax通信でmessage#createを動かす
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -33,18 +30,15 @@ $(function() {
       contentType: false
     })
 
-    // ajax通信が成功
     .done(function(data) {
       var html = buildHTML(data);
       $('.messages').append(html);
       $('#new_message')[0].reset();
       $('.messages').animate( {'scrollTop': $('.messages')[0].scrollHeight}, 1000 );
     })
-    // ajax通信が失敗
     .fail(function(message) {
       alert('メッセージの送信に失敗しました');
     })
-    // ajax通信の成功・失敗にかかわらず作動
     .always(function() {
       $('.form__submit').prop('disabled', false);
     });
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,37 +1,35 @@
 $(function() {
   var search_user = $("#user-search-result");
-
-  // 検索結果が一致したときのHTML
   function appendUser(user) {
-    var html =
-    `<div class="chat-group-user clearfix">
-    <p class="chat-group-user__name">${ user.name }</p>
-    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
-    </div>`
+    var html = `
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${ user.name }</p>
+        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+    </div>
+    `
     search_user.append(html);
   }
 
-  // 検索結果が一致しないときのHTML
   function appendNoUser(user) {
-    var html =
-    `<div class="chat-group-user clearfix">
-    <p class="chat-group-user__name">${ user }</p>
-    </div>`
+    var html = `
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${ user }</p>
+    </div>
+    `
     search_user.append(html);
   }
 
-  // ユーザ追加するときのHTML
   function appendMember(id, name) {
-    var html =
-    `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ id }'>
-    <input name='group[user_ids][]' type='hidden' value='${ id }'>
-    <p class='chat-group-user__name'>${ name }</p>
-    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id='${ id }'>削除</a>
-    </div>`
+    var html = `
+    <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ id }'>
+      <input name='group[user_ids][]' type='hidden' value='${ id }'>
+      <p class='chat-group-user__name'>${ name }</p>
+      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id='${ id }'>削除</a>
+    </div>
+    `
     return html
   }
 
-  // ajax通信でusers#indexを動かす
   $('#user-search-field').on('keyup', function() {
     var input = $('#user-search-field').val();
     $.ajax({
@@ -41,7 +39,6 @@ $(function() {
       dataType: 'json'
     })
 
-    // ajax通信が成功
     .done(function(users){
       $("#user-search-result").empty();
       if ( users.length !== 0 ) {
@@ -52,12 +49,10 @@ $(function() {
         appendNoUser("一致するユーザはいません");
       }
     })
-    // ajax通信が失敗
     .fail(function() {
       alert('ユーザの検索に失敗しました')
     });
 
-    // メンバーを追加する
     $('#user-search-result').on('click', '.user-search-add', function() {
       var id = $(this).data('userId')
       var name = $(this).data('userName');
@@ -66,7 +61,6 @@ $(function() {
       $(this).parent('.chat-group-user').remove();
     });
 
-    // メンバーを削除する
     $('#chat-group-users').on('click', '.user-search-remove', function() {
       var id = $(this).data('userId');
       $(`#chat-group-user-${id}`).remove();
